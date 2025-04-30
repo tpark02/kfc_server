2abf49c079d0b718a550930afd04891a29967afa
tpark02 <tpark3546@gmail.com>
2025-04-30 15:05:34 +0900

    formation positions modified

--- src/main/java/com/example/kfc/data/FormationPositionCounts.java
+++ src/main/java/com/example/kfc/data/FormationPositionCounts.java
@@ -11,140 +11,145 @@ public class FormationPositionCounts {
         FORMATION_POSITION_COUNTS = new HashMap<>();
 
         FORMATION_POSITION_COUNTS.put("442", Map.of(
-                "ST", 2,
-                "LM", 1,
-                "RM", 1,
-                "CAM", 2,
+                "GK", 1,
                 "LB", 1,
                 "CDM", 2,
                 "RB", 1,
-                "GK", 1
+                "LM", 1,
+                "CAM", 2,
+                "RM", 1,
+                "ST", 2
                                                    ));
 
         FORMATION_POSITION_COUNTS.put("433", Map.of(
-                "ST", 2,
-                "RW", 1,
-                "LM", 1,
-                "CM", 1,
-                "RM", 1,
+                "GK", 1,
                 "LB", 1,
-                "CB", 2,
+                "CM", 2,
                 "RB", 1,
-                "GK", 1
+                "CAM", 3,
+                "LW", 1,
+                "ST", 1,
+                "RW", 1
                                                    ));
 
         FORMATION_POSITION_COUNTS.put("4231", Map.of(
-                "ST", 1,
-                "CAM", 3,
-                "CDM", 2,
-                "LB", 1,
-                "CB", 2,
+                "GK", 1,
+                "LB", 2,
+                "RM", 2,
                 "RB", 1,
-                "GK", 1
+                "CDM", 2,
+                "LM", 1,
+                "CAM", 1,
+                "ST", 1
                                                     ));
 
         FORMATION_POSITION_COUNTS.put("451", Map.of(
-                "ST", 1,
-                "LM", 2,
-                "CAM", 1,
+                "GK", 1,
+                "LB", 2,
                 "RM", 2,
-                "LB", 1,
-                "CB", 2,
                 "RB", 1,
-                "GK", 1
+                "LM", 1,
+                "CM", 2,
+                "CAM", 1,
+                "ST", 1
                                                    ));
 
         FORMATION_POSITION_COUNTS.put("343", Map.of(
+                "GK", 1,
+                "LB", 1,
+                "CB", 1,
+                "RM", 2,
+                "LM", 1,
+                "CM", 2,
                 "LW", 1,
                 "ST", 1,
-                "RW", 1,
-                "LM", 1,
-                "CAM", 2,
-                "RM", 1,
-                "CB", 3,
-                "GK", 1
+                "RW", 1
                                                    ));
 
         FORMATION_POSITION_COUNTS.put("352", Map.of(
-                "ST", 2,
-                "LM", 1,
-                "CM", 3,
-                "RM", 1,
-                "CB", 3,
-                "GK", 1
+                "GK", 1,
+                "LB", 1,
+                "CB", 1,
+                "RB", 1,
+                "LM", 2,
+                "CM", 1,
+                "RM", 2,
+                "ST", 2
                                                    ));
 
         FORMATION_POSITION_COUNTS.put("532", Map.of(
-                "ST", 2,
-                "CM", 3,
-                "LB", 1,
-                "CB", 3,
+                "GK", 1,
+                "LB", 2,
+                "CB", 1,
+                "RM", 2,
                 "RB", 1,
-                "GK", 1
+                "LM", 1,
+                "CM", 1,
+                "ST", 2
                                                    ));
 
         FORMATION_POSITION_COUNTS.put("541", Map.of(
-                "ST", 1,
-                "LM", 1,
-                "CM", 2,
-                "RM", 1,
-                "LB", 1,
-                "CB", 3,
+                "GK", 1,
+                "LB", 2,
+                "CB", 1,
+                "RM", 3,
                 "RB", 1,
-                "GK", 1
+                "LM", 2,
+                "ST", 1
                                                    ));
 
         FORMATION_POSITION_COUNTS.put("41212", Map.of(
-                "ST", 2,
-                "CAM", 1,
-                "CM", 2,
-                "CDM", 1,
-                "LB", 2,
-                "CB", 1,
+                "GK", 1,
+                "LB", 1,
+                "LM", 2,
+                "RM", 2,
                 "RB", 1,
-                "GK", 1
+                "CDM", 1,
+                "CAM", 1,
+                "ST", 2
                                                      ));
 
         FORMATION_POSITION_COUNTS.put("4222", Map.of(
-                "ST", 2,
-                "CAM", 2,
-                "CDM", 2,
+                "GK", 1,
                 "LB", 2,
-                "RB", 2,
-                "GK", 1
+                "RB", 1,
+                "RM", 1,
+                "CDM", 2,
+                "CAM", 2,
+                "ST", 2
                                                     ));
 
         FORMATION_POSITION_COUNTS.put("4321", Map.of(
-                "ST", 1,
-                "CAM", 2,
-                "CM", 3,
-                "LB", 1,
-                "CB", 2,
+                "GK", 1,
+                "LB", 2,
+                "RM", 3,
                 "RB", 1,
-                "GK", 1
+                "LM", 2,
+                "CM", 1,
+                "ST", 1
                                                     ));
 
         FORMATION_POSITION_COUNTS.put("4132", Map.of(
-                "ST", 2,
-                "LM", 1,
-                "CAM", 1,
-                "RM", 1,
-                "CDM", 1,
-                "LB", 1,
-                "CB", 2,
+                "GK", 1,
+                "LB", 2,
+                "RM", 2,
                 "RB", 1,
-                "GK", 1
+                "CDM", 1,
+                "LM", 1,
+                "CM", 1,
+                "ST", 2
                                                     ));
 
         FORMATION_POSITION_COUNTS.put("424", Map.of(
-                "LW", 1,
-                "ST", 2,
-                "RW", 1,
-                "CM", 2,
+                "GK", 1,
                 "LB", 1,
-                "CB", 2,
+                "LM", 2,
+                "RM", 2,
                 "RB", 1,
-                "GK", 1
+                "LW", 1,
+                "RW", 1,
+                "ST", 2
                                                    ));
     }
+
 }
