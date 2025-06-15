99f38b61c0596e1dc3a4c6eb21b2d0fa155654cd
tpark02 <tpark3546@gmail.com>
2025-06-15 21:02:18 +0900

    PlayerPageResponse variable name modified

--- src/main/java/com/example/kfc/Response/PlayerPageResponse.java
+++ src/main/java/com/example/kfc/Response/PlayerPageResponse.java
@@ -8,14 +8,14 @@ import java.util.List;
 @Getter
 public class PlayerPageResponse {
 
-    private final List<PlayerDto> content;
+    private final List<PlayerDto> playerList;
     private final int number;
     private int size;
     private int totalPages;
     private long totalElements;
 
-    public PlayerPageResponse(List<PlayerDto> content, int number, int size, int totalPages, long totalElements) {
-        this.content = content;
+    public PlayerPageResponse(List<PlayerDto> playerDtoList, int number, int size, int totalPages, long totalElements) {
+        this.playerList = playerDtoList;
         this.number = number;
         this.size = size;
         this.totalPages = totalPages;
